332c55c2663e9e9f6db8d047f803b9fc348e5123
Travis Martin <tjmartin2003@gmail.com>
2024-07-10 03:42:52 -0500

    Dependency updates.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Test](https://github.com/Lakuna/react-canvas/actions/workflows/test.yml/badge.svg)](https://github.com/Lakuna/react-canvas/actions/workflows/test.yml)
+
 # React Canvas
 
 A React canvas element with a built-in animation.
--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,51 @@
 import {
-	useEffect,
-	useRef,
-	type DetailedHTMLProps,
 	type CanvasHTMLAttributes,
-	type MutableRefObject,
+	type DetailedHTMLProps,
 	useCallback,
-	type JSX,
-	type EffectCallback,
-	default as React
+	useEffect,
+	useRef
 } from "react";
 
 /**
  * A React canvas element with a built-in animation.
- * @param initializationStep The initialization step, which is executed once when the animation starts. It should return a function representing the render step, which is executed once for every frame in the animation.
- * @param properties The properties to apply to the canvas.
+ * @param init - The initialization step, which is executed once when the animation starts. It should return a function representing the render step, which is executed once for every frame in the animation.
+ * @param props - The properties to apply to the canvas.
  * @returns The canvas.
  */
 export default function ReactCanvas(
-	initializationStep: (canvas: HTMLCanvasElement) => FrameRequestCallback,
-	properties: DetailedHTMLProps<
+	init: (canvas: HTMLCanvasElement) => FrameRequestCallback,
+	props: DetailedHTMLProps<
 		CanvasHTMLAttributes<HTMLCanvasElement>,
 		HTMLCanvasElement
 	>
-): JSX.Element {
-	const canvas: MutableRefObject<HTMLCanvasElement | null> = useRef(null);
-	const renderStep: MutableRefObject<((now: number) => void) | null> =
-		useRef(null);
-	const doDisableCanvas: MutableRefObject<boolean> = useRef(false);
+) {
+	const canvas = useRef(null as HTMLCanvasElement | null);
+	const renderStep = useRef(null as FrameRequestCallback | null);
+	const doDisableCanvas = useRef(false);
 
-	const tick: FrameRequestCallback = useCallback((now: number): void => {
-		if (doDisableCanvas.current) {
-			return;
-		}
-
-		if (!renderStep.current) {
+	const tick = useCallback((time: DOMHighResTimeStamp): void => {
+		if (doDisableCanvas.current || renderStep.current === null) {
 			return;
 		}
 
 		requestAnimationFrame(tick);
-		renderStep.current(now);
+		renderStep.current(time);
 	}, []);
 
-	const effectCallback: EffectCallback = (): void | (() => void) => {
-		if (!canvas.current) {
-			return;
+	const effectCallback = () => {
+		if (canvas.current === null) {
+			return () => void 0;
 		}
 
-		renderStep.current = initializationStep(canvas.current);
+		renderStep.current = init(canvas.current);
 		requestAnimationFrame(tick);
 
-		return (): void => {
+		return () => {
 			doDisableCanvas.current = true;
 		};
 	};
 
-	useEffect(effectCallback, [initializationStep, tick]);
+	useEffect(effectCallback, [init, tick]);
 
-	return <canvas ref={canvas} {...properties} />;
+	return <canvas ref={canvas} {...props} />;
 }
