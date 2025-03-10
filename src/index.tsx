7ab42a190effe5b510bc97898afca38a96c75d04
Travis Martin <tjmartin2003@gmail.com>
2025-03-09 22:31:22 -0500

    Dependency and typings update.

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,25 @@
 import { type JSX, useCallback, useEffect, useRef } from "react";
 
-/** The props that can be applied to a `ReactCanvas`. */
-export type ReactCanvasProps = JSX.IntrinsicElements["canvas"] & {
+/**
+ * The props that can be applied to a `ReactCanvas`.
+ * @public
+ */
+export interface ReactCanvasProps
+	extends Omit<JSX.IntrinsicElements["canvas"], "ref"> {
 	/** The initialization step, which is executed once when the animation starts. It should return a function representing the render step, which is executed once for every frame in the animation. */
 	init: (canvas: HTMLCanvasElement) => FrameRequestCallback;
-};
+}
 
 /**
  * A React canvas element with a built-in animation.
  * @param props - The properties to apply to the canvas.
  * @returns The canvas.
+ * @public
  */
 export default function ReactCanvas({
 	init,
-	ref,
 	...props
 }: ReactCanvasProps): JSX.Element {
-	// Disregard the passed `ref` so that it doesn't conflict with the built-in `canvas` ref.
-	void ref;
-
 	const canvas = useRef(null as HTMLCanvasElement | null);
 	const renderStep = useRef(null as FrameRequestCallback | null);
 	const doDisableCanvas = useRef(false);
